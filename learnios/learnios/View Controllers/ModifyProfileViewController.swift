00032c296e2c3ab3d222c65064742511f120b9f0
Linli <lw2780@columbia.edu>
2018-03-30 10:45:44 -0400

    modify profile

---
+++ learnios/learnios/View Controllers/ModifyProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ModifyProfileViewController.swift
+//  learnios
+//
+//  Created by 万琳莉 on 30/03/2018.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ModifyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- learnios/learnios/View Controllers/UserProfileViewController.swift
+++ learnios/learnios/View Controllers/UserProfileViewController.swift
@@ -12,6 +12,9 @@ import FirebaseAuth
 class UserProfileViewController: UIViewController {
     @IBOutlet weak var LogoutButton: UIButton!
     @IBOutlet weak var ProfilePicture: UIImageView!
+    @IBOutlet weak var MyName: UILabel!
+    @IBOutlet weak var MyEmail: UILabel!
+    @IBOutlet weak var MyLocation: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         let db = Firestore.firestore()
@@ -19,7 +22,11 @@ class UserProfileViewController: UIViewController {
         let email = Auth.auth().currentUser?.email
         let docRef = db.collection("users").document(email!);
         //let URL = docRef.value(forKeyPath: "avatar")
-        
+//        MyName.text = "null"
+////        MyEmail.text = String(describing: email)
+//        MyEmail.text = "null"
+//        MyLocation.text = "null"
+//        fillItems()
         let httpsReference = storage.reference(forURL: "gs://coopmart-1f06f.appspot.com/test1.jpeg")
         httpsReference.getData(maxSize: 10000 * 10000 * 10000){ data, error in
             if let error = error {
@@ -33,6 +40,20 @@ class UserProfileViewController: UIViewController {
         }
         LogoutButton.addTarget(self, action: #selector(LogoutButtonTapped), for: .touchUpInside)
     }
+    
+//    func fillItems (){
+//        MyName.text = "Linli"
+//    }
+//    // instantiate a inputs container view
+//    let inputsContainerView: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = UIColor.white
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.layer.cornerRadius = 5
+//        view.layer.masksToBounds = true
+//        return view
+//    }()
+    
      @objc func LogoutButtonTapped(sender: UIButton) {
         try! Auth.auth().signOut()
         let loginController = LoginController()
