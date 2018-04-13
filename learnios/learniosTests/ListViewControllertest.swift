4ffc87640b7f58ba98b5176b9e283aa64de58d14
zyy <jy2913@columbia.edu>
2018-04-13 12:07:09 -0400

    UI test and unit test

--- learnios/learniosTests/ListViewControllertest.swift
+++ learnios/learniosTests/ListViewControllertest.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-
-class ListViewControllertest: AUAudioUnitBus {
-    
+@testable import learnios
+class ListViewControllertest: XCTestCase {
+    var systemUnderTest: ListingViewController!
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
